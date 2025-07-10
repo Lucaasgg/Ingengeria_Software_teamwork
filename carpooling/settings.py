a949608b20de4e2be1a9bdbaf201baa5f613cabb
Lucas Garcia <lukggpalomo@gmail.com>
2025-07-10 15:45:12 +0200

    created login.html template and modified settings.py to include rides app

--- carpooling/settings.py
+++ carpooling/settings.py
@@ -52,21 +52,23 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'carpooling.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
+ TEMPLATES = [
+     {
+         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        # Look in the project-level “templates/” folder first
+        'DIRS': [ BASE_DIR / 'templates' ],
+         'APP_DIRS': True,
+         'OPTIONS': {
+             'context_processors': [
+                 'django.template.context_processors.debug',
+                 'django.template.context_processors.request',
+                 'django.contrib.auth.context_processors.auth',
+                 'django.contrib.messages.context_processors.messages',
+             ],
+         },
+     },
+ ]
+
 
 WSGI_APPLICATION = 'carpooling.wsgi.application'
 
